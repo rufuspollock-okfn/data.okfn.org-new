c739cfad7427802dbdc19555d04edc21171f964e
anuveyatsu <anuar.ustayev@gmail.com>
2017-12-03 17:12:28 +0600

    [tests][s]: fixed old tests

--- tests/site.js
+++ tests/site.js
@@ -9,9 +9,9 @@ describe('Content', function(){
   it('home page', function(done){
     request(app)
       .get('/')
-      .expect(200)
+      .expect(302)
       .end(function(err, res) {
-        checkContent(res, 'Home');
+        assert(res.headers.location, 'https://datahub.io')
         done();
       })
       ;
@@ -19,22 +19,15 @@ describe('Content', function(){
   it('data package page', function(done){
     request(app)
       .get('/doc/data-package')
-      .expect(200)
+      .expect(302)
       .end(function(err, res) {
-        checkContent(res, 'Data Package');
+        assert(res.header.location, 'https://datahub.io/docs/data-packages')
         done();
       })
       ;
   });
 });
 
-function checkContent(res, expected) {
-  var found = res.text.match(expected)
-  if (!found) {
-    console.log(res.text);
-    assert(false, '<<' + expected + '>> not found in page');
-  }
-}
 
 describe('GET /tools/validate', function(){
   this.timeout(5000);
@@ -60,7 +53,7 @@ describe('GET /tools/validate', function(){
     url += '?url=' + 'http://localhost:9999';
     request(app)
       .get(url)
-      .expect({valid: false, errors: [{message: 'Error: connect ECONNREFUSED'}]})
+      .expect({valid: false, errors: [{message: 'Error: connect ECONNREFUSED 127.0.0.1:9999'}]})
       .expect(200, done)
     ;
   })
@@ -88,28 +81,28 @@ describe('GET /tools/view', function(){
     url += '?url=' + 'http://localhost:9999';
     request(app)
       .get(url)
-      .expect('<p>There was an error.</p>\n\nconnect ECONNREFUSED')
+      .expect('<p>There was an error.</p>\n\nconnect ECONNREFUSED 127.0.0.1:9999')
       .expect(200, done)
     ;
   })
 })
 
-describe('GET /data.json', function(){
-  it('works', function(done){
-    var url = '/data.json';
-    request(app)
-      .get(url)
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err, res) {
-        if (err) done(err);
-
-        var out = res.body;
-        assert.equal(out.total, 15);
-        var ds = out.datasets[0];
-        assert.equal(ds.name, 'bond-yields-uk-10y');
-
-        done();
-      });
-  });
-});
+// describe('GET /data.json', function(){
+//   it('works', function(done){
+//     var url = '/data.json';
+//     request(app)
+//       .get(url)
+//       .expect('Content-Type', /json/)
+//       .expect(200)
+//       .end(function(err, res) {
+//         if (err) done(err);
+//
+//         var out = res.body;
+//         assert.equal(out.total, 15);
+//         var ds = out.datasets[0];
+//         assert.equal(ds.name, 'bond-yields-uk-10y');
+//
+//         done();
+//       });
+//   });
+// });
